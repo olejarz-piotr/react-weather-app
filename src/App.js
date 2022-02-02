03f933382180324ada641aabaa7b055bf1ec1158
Piotr Olejarz <piotr.olejarz@peak11.com>
2022-02-02 23:44:32 +0100

    adding more css and functionality to main js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 
 const api={
@@ -6,6 +6,18 @@ const api={
   base:'https://api.openweathermap.org/data/2.5/'
 }
 function App() {
+const [query, setQuery] = useState('');
+const [weather, setWeather]=useState('');
+
+    const dateBuilder = (d)=>{
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+    }
 
   return (
     <div className="app">
@@ -13,6 +25,16 @@ function App() {
    <div className='search-box'>
      <input type='text' className='search-bar' placeholder='Enter name'/>
    </div>
+    <div className='location-box'>
+        <div className='location'>New York City, US</div>
+        <div className='date'>{dateBuilder(new Date())}</div>
+    </div>
+    <div className='weather-box'>
+        <div className='temp'>
+            15C
+        </div>
+        <div className='weather'>Sunny</div>
+    </div>
 </main>
     </div>
   );
